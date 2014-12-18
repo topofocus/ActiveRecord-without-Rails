dae070be45e77e59a1472f5d1467ad78c01589be
Hartmut Bischoff <beta>
2014-12-18 18:29:49 +0100

    Robust Version of Connect.rb
    The connection to the database will be established regardless of the location of the calling script

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,7 +1,7 @@
 class Connection
 
 	def self.connect(environment:'production')
-		ActiveRecord::Base.configurations = ActiveRecord::Tasks::DatabaseTasks.database_configuration = YAML::load(File.open('./config/database.yml'))  # YOUR_CONFIG_HASH_AS_IN_DATABASE_YML
+		ActiveRecord::Base.configurations = ActiveRecord::Tasks::DatabaseTasks.database_configuration = YAML::load File.open( File.expand_path('../../config/database.yml', __FILE__)) # YOUR_CONFIG_HASH_AS_IN_DATABASE_YML
 		ActiveRecord::Tasks::DatabaseTasks.db_dir = 'db' # PATH_TO_YOUR_DB_DIRECTORY
 		ActiveRecord::Tasks::DatabaseTasks.env    = environment # YOUR_ENV_AS_RAILS_ENV
 
